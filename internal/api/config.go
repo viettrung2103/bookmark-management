d3913e185141d83e9e5f774bdfca34227de86026
viettrung2103 <viettrung21.work@gmail.com>
2026-06-03 23:39:43 +0700

    lesson1

---
+++ internal/api/config.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/kelseyhightower/envconfig"
+
+type Config struct {
+	AppPort string `default:"8080" envconfig:"APP_PORT"`
+}
+
+func NewConfig() (*Config, error) {
+	cfg := &Config{}
+	err := envconfig.Process("api", cfg)
+	if err != nil {
+		return nil, err
+	}
+	return cfg, err
+}
