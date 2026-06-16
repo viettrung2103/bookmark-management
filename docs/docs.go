0201ad268c1784a410508816e713358a7edbc301
viettrung2103 <viettrung21.work@gmail.com>
2026-06-16 18:37:16 +0700

    fix docs

--- docs/docs.go
+++ docs/docs.go
@@ -116,7 +116,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "1.0",
+	Version:          "1.5",
 	Host:             "localhost:8080",
 	BasePath:         "/",
 	Schemes:          []string{},
