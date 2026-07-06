c3c3a4f95817e35d75d65425c0d31fd0ace25640
viettrung2103 <viettrung21.work@gmail.com>
2026-07-06 15:09:45 +0700

    add all

--- docs/docs.go
+++ docs/docs.go
@@ -351,7 +351,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "2.5",
+	Version:          "3.0",
 	Host:             "localhost:8080",
 	BasePath:         "",
 	Schemes:          []string{},
