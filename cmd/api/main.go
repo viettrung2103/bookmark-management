7a1d6bce6a490cde58917b0c982cc2bcfa5aaa5b
viettrung2103 <viettrung21.work@gmail.com>
2026-07-29 01:26:30 +0700

    deploy

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/viettrung2103/bookmark-management/internal/infrastructure"
 
 // @title Bookmark API
-// @version 4.0
+// @version 4.0.0
 // @description API for bookmark management
 // @host localhost:8080
 // @securityDefinitions.apikey BearerAuth
