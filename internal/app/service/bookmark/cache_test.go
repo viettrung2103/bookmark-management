482f021857b689b207f4506c8723edee61e859b1
viettrung2103 <viettrung21.work@gmail.com>
2026-07-30 04:25:28 +0700

    finish get test for cache

--- internal/app/service/bookmark/cache_test.go
+++ internal/app/service/bookmark/cache_test.go
@@ -1,1 +1,81 @@
 package bookmark_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/viettrung2103/bookmark-management/internal/app/model"
+	mock_cache "github.com/viettrung2103/bookmark-management/internal/app/repository/cache/mocks"
+	"github.com/viettrung2103/bookmark-management/internal/app/service/bookmark"
+	"github.com/viettrung2103/bookmark-management/internal/app/service/bookmark/mocks"
+	"github.com/viettrung2103/bookmark-management/internal/test/data/fixtures"
+)
+
+const (
+	validCacheGroupKey = "get_bookmarks_133b3b42-70b9-456c-82e7-bf1b570e6c51"
+	validUserID        = "133b3b42-70b9-456c-82e7-bf1b570e6c51"
+	validCacheKey      = "page_1_limit_10"
+)
+
+func TestBookmarkCacheService_GetBookmarks(t *testing.T) {
+	t.Parallel()
+
+	testcases := []struct {
+		name string
+
+		setupService func(ctx context.Context) *mocks.Service
+		setupCache   func(ctx context.Context) *mock_cache.DB
+
+		expectedResult *bookmark.GetBookmarkResult
+		expectedError  error
+	}{
+		{
+			name: "success - with cache",
+			setupService: func(ctx context.Context) *mocks.Service {
+				mockService := mocks.NewService(t)
+				return mockService
+			},
+			setupCache: func(ctx context.Context) *mock_cache.DB {
+				mockCache := mock_cache.NewDB(t)
+				mockCache.On("GetCacheData", ctx, validCacheGroupKey, validCacheKey).Return(
+					[]byte(`{"Bookmarks":[{"id":"f4defa89-c5b3-4f26-8ca1-614495cdde12","description":"Google","url":"https://www.google.com","code":"kAQllBal"},{"id":"f4defa89-c5b3-4f26-8ca1-614495cdde22","description":"Google1","url":"https://www.google1.com","code":"kAQllBal1"}],"Count":2}`),
+					nil,
+				)
+				return mockCache
+			},
+			expectedResult: &bookmark.GetBookmarkResult{
+				Bookmarks: []*model.Bookmark{
+					{
+						Base:        fixtures.GetTestBase("f4defa89-c5b3-4f26-8ca1-614495cdde12"),
+						Description: "Google",
+						URL:         "https://www.google.com",
+						Code:        "kAQllBal",
+					},
+					{
+						Base:        fixtures.GetTestBase("f4defa89-c5b3-4f26-8ca1-614495cdde22"),
+						Description: "Google1",
+						URL:         "https://www.google1.com",
+						Code:        "kAQllBal1",
+					},
+				},
+				Count: 2,
+			},
+		},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctx := t.Context()
+
+			mockService := tc.setupService(ctx)
+			mockCache := tc.setupCache(ctx)
+
+			bookmarkCacheService := bookmark.NewBookmarkCacheService(mockService, mockCache)
+			res, err := bookmarkCacheService.GetBookmarks(ctx, validUserID, 1, 10)
+			assert.Equal(t, tc.expectedResult, res)
+			assert.Equal(t, tc.expectedError, err)
+		})
+	}
+}
